6ca1e17dbb85626d5658f0cde14f76a2b4ac9d80
issameddinelabidi <issameddinelabidi@gmail.com>
2025-05-13 22:27:06 +0200

    Initial commit

---
+++ resources/js/Components/InputError.vue
@@ -0,0 +1,11 @@
+<template>
+    <div v-if="formError" class="input-error">
+        {{ formError }}
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    formError: String
+})
+</script>
---
+++ resources/js/pages/Index/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Index</div>
+    <Link href="/hello">Show Page</Link>
+    <div>This message is {{ message }}</div>
+</template>
+
+<script setup>
+import {Link} from '@inertiajs/vue3';
+
+defineProps({
+    message: String
+})
+</script>
